6794fbff375de1c8816460e15f62b81c8cc5e220
murilo-marian <murilomarian.mm@gmail.com>
2023-09-05 15:40:11 -0300

    changed server to singleton

---
+++ src/Server.java
@@ -0,0 +1,62 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server implements Runnable {
+    private ServerSocket servidor;
+    private Socket conexao = null;
+    private List<ConnectionHandler> clientes = new ArrayList<>();
+    private static Server server;
+
+
+    @Override
+    public void run() {
+
+        try {
+
+            while (true) {
+                conexao = servidor.accept();
+                ConnectionHandler connectionHandler = new ConnectionHandler(conexao);
+                Object lock = new Object();
+                synchronized (lock) {
+                    clientes.add(connectionHandler);
+                }
+                for (ConnectionHandler cliente : clientes) {
+                    cliente.setClientes(clientes);
+                }
+                new Thread(connectionHandler).start();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Algo de errado aconteceu");
+        } finally {
+            //encerro o socket de comunicação
+            try {
+                conexao.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                servidor.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private Server(int porta) throws IOException {
+        this.servidor = new ServerSocket(porta);
+    }
+
+    public static synchronized Server getInstance(int porta) throws IOException {
+        if (server == null)
+            server = new Server(porta);
+
+        return server;
+    }
+
+}
--- src/ServidorSocket.java
+++ src/ServidorSocket.java
@@ -5,48 +5,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ServidorSocket {
-    //criar um chat utilizando esse conceito, que funcione usando socket
 
-    //socket:
-    //IP
-    //PROTOCOLO
-    //PORTA
 
     public static void main(String[] args) throws IOException {
-        //Cria o servidor e seta como nulo
-        ServerSocket servidor = null;
-
-        //cria uma conexão para atender o cliente e seta como nulo
-        Socket conexao = null;
-
-        //entrada de dados
-        BufferedReader entrada = null;
-
-        try {
-            servidor = new ServerSocket(7000);
-            List<ConnectionHandler> clientes = new ArrayList<>();
-
-            while (true) {
-                conexao = servidor.accept();
-                ConnectionHandler connectionHandler = new ConnectionHandler(conexao);
-                Object lock = new Object();
-                synchronized (lock) {
-                    clientes.add(connectionHandler);
-                }
-                for (ConnectionHandler cliente : clientes) {
-                    cliente.setClientes(clientes);
-                }
-                new Thread(connectionHandler).start();
-            }
-
-        } catch (IOException e) {
-            System.out.println("Algo de errado aconteceu");
-        } finally {
-            //encerro o socket de comunicação
-            conexao.close();
-
-            servidor.close();
-        }
+    Server server = Server.getInstance(7000);
+    server.run();
     }
 
 
